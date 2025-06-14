7313895f4bc23a7ba774cb58f783d21699bed24c
min5ol <sol0508@kakao.com>
2025-06-14 21:32:16 +0900

    Initial commit: frontend + backend

---
+++ modo-hamkke-back/modohamkke/src/main/java/com/modobeat/modohamkke/ModohamkkeApplication.java
@@ -0,0 +1,13 @@
+package com.modobeat.modohamkke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModohamkkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModohamkkeApplication.class, args);
+    }
+
+}
